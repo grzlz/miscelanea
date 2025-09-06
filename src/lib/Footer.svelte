e65aa3ae8787e054fe61deb47c37fca8a4aab3a6
grzlz <guillermo.rdzlpz@gmail.com>
2025-09-06 16:31:22 -0600

    Automatic deploy

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class="bg-gray-900 text-white py-8 px-4">
+	<div class="max-w-7xl mx-auto text-center">
+		<p class="text-gray-300">
+			Hecho con <span class="text-red-500 text-xl">♥</span> por <a href="https://icarus.mx" class="text-amber-500 hover:text-amber-400 transition-colors font-medium">icarus.mx</a>
+		</p>
+	</div>
+</footer>
